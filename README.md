5331172f6f4aa3c6ff3a6258ab1190eae5b9dee6
HenryLi-0 <bxsci8266@gmail.com>
2024-06-08 18:16:16 -0400

    06082024 (afternoon) - silly bezier curve path

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 
 Issue: No editing software I have can easily move solid character images around the screen
 
-Normal Solution: Download a suspicious editing software to test this out
+**Normal Solution:** Download an editing software with this capability and test it out
 
-My Solution: Decide to make a video editor
+**My Solution:** Decide to make a video editor
 
 One option is clearly better here, you decide which. (joke)
 
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from subsystems.pathing import path3coords
+from subsystems.pathing import beizerPath3coords, beizerPathCoords
 import time
 import random
 
-gen = lambda: 2500-random.random()*5000
+gen = lambda: 180-random.random()*360
 steps = 60
 
-tests = 0
-total = 0
+# tests = 0
+# total = 0
 
-while tests<1000:
-    coords = [(gen(),gen()),(gen(),gen()),(gen(),gen())]
-    start = time.time()
-    path3coords(coords,steps)
-    end = time.time()
-    total+=end-start
-    tests+=1
+# while tests<1000:
+#     coords = [(gen(),gen()),(gen(),gen()),(gen(),gen())]
+#     start = time.time()
+#     beizerPath3coords(coords,steps)
+#     end = time.time()
+#     total+=end-start
+#     tests+=1
 
-    print(f"{tests} tests | avg: {total/tests} seconds")
+#     print(f"{tests} tests | avg: {total/tests} seconds")
 
+thing = beizerPathCoords([(0,0),(100.01,100.01),(-100.02,100.02),(-100.03,-100.03),(100.04,-100.04),(0.05,0.05)],30)
+
+for item in thing:
+    print(item[0])
+    print(item[1])
